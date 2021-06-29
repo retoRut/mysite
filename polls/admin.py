d6365ca99fbc0d8466c6c678e1f7c063050399bf
reto <info@rutishauser-ag.ch>
2021-06-29 19:39:53 +0200

    developp

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
+# import django_admin_listfilter_dropdown.filters
 
-from .models import Question, Mieter, Mietobjekt, Nebenkosten, Nebenkosten_Typ, Kosten, Month, Year, Mietzinseingaenge, \
-    ZahlungsTyp, Mietzinse
+from .models import Question, Mieter, Mietobjekt, Nebenkosten, Nebenkosten_Typ, Kosten, Mietzinseingaenge, \
+ Mietzins, Year
 
 admin.site.register(Question)
 
@@ -16,22 +17,41 @@ class Nebenkosten_TypAdmin(admin.ModelAdmin):
 class MieterAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Mieter._meta.fields]
 
-#admin.site.register(Mieter)
-admin.site.register(Mietobjekt)
-admin.site.register(Nebenkosten)
-admin.site.register(Mietzinse)
+admin.site.register(Year)
+
+
+@admin.register(Mietobjekt)
+class MietobjektAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Mietobjekt._meta.fields]
+# admin.site.register(Nebenkosten)
+# ---- Table Mietzinseingaenge
+@admin.register(Nebenkosten)
+class NebenkostenAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Nebenkosten._meta.fields]
+
+@admin.register(Mietzins)
+class MietzinsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Mietzins._meta.fields]
+
 # admin.site.register(Nebenkosten_Typ)
 admin.site.register(Kosten)
-admin.site.register(Month)
-admin.site.register(Year)
-admin.site.register(ZahlungsTyp)
+# admin.site.register(Month)
 
 # ---- Table Mietzinseingaenge
 @admin.register(Mietzinseingaenge)
 class MietzinseingaengeAdmin(admin.ModelAdmin):
-    list_display = ("mieter", "betrag","datum","month","year" )
+    list_display = ("mieter", "betrag","datum","month")
 
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        print(str(db_field))
+        if db_field.name == "nebenkosten":
+            kwargs["queryset"] = Nebenkosten.objects.filter(aktiv=True).filter(mieter=7)
+        return super(MietzinseingaengeAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
-
+ #   list_filter = (("mieter", RelatedDropdownFilter),)
+ #   def get_form(self, request, obj=None, **kwargs):
+ #       if obj is not None and obj.type is not None:
+ #           kwargs['form'] = tagform_factory(obj.type)
+ #       return super(TagAdmin, self).get_form(request, obj, **kwargs)
 
 
--- polls/models.py
+++ polls/models.py
@@ -24,62 +24,31 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-
-class Mieter(models.Model):
-    company = models.CharField(verbose_name="Firma", max_length=200,blank = True)
-    first_name = models.CharField(verbose_name="Vorname",max_length=200)
-    last_name = models.CharField(verbose_name="Nachname", max_length=200)
-    phone_number = models.CharField(verbose_name="Telefonnummer", max_length=200)
-    conto_number = models.CharField(verbose_name="IBAN Nummer", max_length=200, default=0)
-    address = models.CharField(verbose_name="Strasse", max_length=200, default=0)
-    address_Nr = models.CharField(verbose_name="Strassennummer",max_length=200, default=0)
-    city = models.CharField(verbose_name="Wohnort", max_length=200, default=0)
-    city_code = models.IntegerField(verbose_name="PLZ", default=0)
-    first_name_2nd = models.CharField(verbose_name="Zweitmieter Vorname", max_length=200,blank = True)
-    last_name_2nd = models.CharField(verbose_name="Zweitmieter Nachname", max_length=200, blank = True)
-    activ = models.BooleanField(verbose_name="aktiv", default=False)
-
-    def __str__(self):
-        """
-            used for headlines
-        :return:
-        """
-        return ('{} {} {} {} {} {} {} {} {}').format(
-                                                    self.company,
-                                                    self.first_name,
-                                                    self.last_name,
-                                                    self.first_name_2nd,
-                                                    self.first_name_2nd,
-                                                    self.phone_number,
-                                                    self.address,
-                                                    self.address_Nr,
-                                                    self.city,
-                                                    self.city_code)
-
-class Mietzinse(models.Model):
-    #mietobjekt = models.ForeignKey(Mietobjekt, on_delete=models.CASCADE)
-    description = models.CharField(verbose_name="Beschreibung",max_length=200)
-    rent = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.description
-
 class Mietobjekt(models.Model):
     name = description = models.CharField(verbose_name="Objektname",max_length=200, default=0)
-    mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE)
-    mietzins = models.ForeignKey(Mietzinse, on_delete=models.CASCADE, default=0)
     # mietzins = models.ManyToManyField(Mietzinse)
-    description = models.CharField(verbose_name="Beschreibung",max_length=200)
-    rent = models.IntegerField(default=0)
-    a_conto = models.IntegerField(default=0)
+    #description = models.CharField(verbose_name="Beschreibung",max_length=1000)
+    description = models.TextField(verbose_name="Beschreibung", max_length=1024 * 2)
+    # rent = models.DecimalField(max_digits=6, decimal_places=2)
+    photo = models.ImageField(verbose_name="Bild", default=0)
 
     def __str__(self):
         return self.description
 
 
+class Mietzins(models.Model):
+    #mietobjekt = models.ForeignKey(Mietobjekt, on_delete=models.CASCADE)
+    description = models.CharField(verbose_name="Beschreibung",max_length=200)
+    rent = models.DecimalField(verbose_name="Miete",max_digits=6, decimal_places=2)
+    # mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE, default=0)
+    mietobject = models.ForeignKey(Mietobjekt, on_delete=models.CASCADE, default=0)
+    aktiv = models.BooleanField(default=False)
+    def __str__(self):
+        return str(self.rent)
 
-class ZahlungsTyp(models.Model):
-    typ = models.CharField(max_length=50, default = '1' )
+
+class Year(models.Model):
+    typ = models.IntegerField(default=2010)
 
     def __str__(self):
         return str(self.typ)
@@ -88,14 +57,14 @@ class Nebenkosten_Typ(models.Model):
     # set headline
     typ = models.CharField(max_length=200)
     # a_conto = models.BooleanField(default=False)
-    a_conto = models.ForeignKey(ZahlungsTyp, on_delete=models.CASCADE)
+    a_conto =  models.CharField(max_length=10, choices=(('a', 'à conto',),
+                                         ('p', 'pauschal')))
 
     def __str__(self):
         return self.typ + " "+ str(self.a_conto)
 
-
 class Nebenkosten(models.Model):
-    mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE)
+    # mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE)
     typ = models.ForeignKey(Nebenkosten_Typ, on_delete=models.CASCADE)
     betrag = models.DecimalField(max_digits=6, decimal_places=2)
     aktiv = models.BooleanField(default=False)
@@ -104,6 +73,37 @@ class Nebenkosten(models.Model):
 
     def __str__(self):
         return str(self.betrag)+' '+str(self.aktiv)
+
+class Mieter(models.Model):
+    company = models.CharField(verbose_name="Firma", max_length=200,blank = True)
+    first_name = models.CharField(verbose_name="Vorname",max_length=200)
+    last_name = models.CharField(verbose_name="Nachname", max_length=200)
+    phone_number = models.CharField(verbose_name="Telefonnummer", max_length=200)
+    conto_number = models.CharField(verbose_name="IBAN Nummer", max_length=200, default=0)
+    address = models.CharField(verbose_name="Strasse", max_length=200, default=0)
+    address_Nr = models.IntegerField(verbose_name="Strassennummer", default=0)
+    city = models.CharField(verbose_name="Wohnort", max_length=200, default=0)
+    city_code = models.IntegerField(verbose_name="PLZ", default=0)
+    first_name_2nd = models.CharField(verbose_name="Zweitmieter Vorname", max_length=200,blank = True)
+    last_name_2nd = models.CharField(verbose_name="Zweitmieter Nachname", max_length=200, blank = True)
+    activ = models.BooleanField(verbose_name="aktiv", default=False)
+    mietzins = models.ManyToManyField(Mietzins)
+    nebenkosten = models.ManyToManyField(Nebenkosten)
+
+    def __str__(self):
+        """
+            used for ForeignKey dropDown
+        :return:
+        """
+        m_zins = ", ".join(str(m) for m in self.mietzins.all())
+        return ('{} {} {} {} ').format(
+                                                    self.company,
+                                                    self.first_name,
+                                                    self.last_name,
+                                                    m_zins)
+
+
+
 
 
 class Kosten(models.Model):
@@ -115,28 +115,25 @@ class Kosten(models.Model):
         return str(self.mieter)+' '+str(self.miete)+' '+str(self.nebenkosten)
 
 
-class Month(models.Model):
-    month = models.CharField(max_length=50)
-
-    def __str__(self):
-        return str(self.month)
-
-
-class Year(models.Model):
-    year = models.CharField(max_length=50)
-
-    def __str__(self):
-        return str(self.year)
-
-
 
 class Mietzinseingaenge(models.Model):
     datum = models.DateField(default=datetime.now())
     # ,input_formats=["%Y-%m-%d", ]
-    month = models.ForeignKey(Month, on_delete=models.CASCADE)
-    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    month = models.CharField(max_length=10, choices=(('JA', 'Januar',),
+                                         ('FE', 'Februar'),
+                                         ('MA', 'März'),
+                                         ('AP', 'April'),
+                                         ('MA', 'Mai'),
+                                         ('JN', 'Juni'),
+                                         ('JU', 'Juli'),
+                                         ('AU', 'August'),
+                                         ('SE', 'September'),
+                                         ('OK', 'Oktober'),
+                                         ('NO', 'November'),
+                                         ('DE', 'Dezember'),))
     mieter = models.ForeignKey(Mieter, on_delete=models.CASCADE)
     betrag = models.IntegerField(default=0)
+    year  = models.ForeignKey(Year, on_delete=models.CASCADE, default=2010)
 
 
     def __str__(self):
